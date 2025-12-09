640d3ca00383944e45c40ae19babc07d6294de18
zunf <1430119384@qq.com>
2025-12-09 23:45:49 +0800

    first commit

---
+++ src/main/java/com/zunf/tankbattletcpserver/TankBattleTcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.zunf.tankbattletcpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TankBattleTcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TankBattleTcpServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zunf/tankbattletcpserver/TankBattleTcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zunf.tankbattletcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TankBattleTcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
