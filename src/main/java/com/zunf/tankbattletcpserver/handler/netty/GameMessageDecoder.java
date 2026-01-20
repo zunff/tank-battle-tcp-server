d7d525cafe2a590bca4856f044b210704298aaf0
zunf <1430119384@qq.com>
2026-01-20 22:56:49 +0800

    fix(netty): 修复GameMessageDecoder中的内存泄漏导致老年代持续飙升问题
    - 在GameMessageDecoder中添加try-finally块确保ByteBuf被正确释放

--- src/main/java/com/zunf/tankbattletcpserver/handler/netty/GameMessageDecoder.java
+++ src/main/java/com/zunf/tankbattletcpserver/handler/netty/GameMessageDecoder.java
@@ -6,6 +6,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageDecoder;
+import io.netty.util.ReferenceCountUtil;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -16,21 +17,24 @@ public class GameMessageDecoder extends MessageToMessageDecoder<ByteBuf> {
 
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf buf, List<Object> out) throws Exception {
-        // 解析头部
-        int typeByte = buf.readUnsignedShort();
-        byte version = buf.readByte();
-        int requestId = buf.readInt();
-        int bodyLength = buf.readInt();
-        int crc32 = buf.readInt(); // 这里可以不用了，只是把 readerIndex 往后挪
+        try {
+            // 解析头部
+            int typeByte = buf.readUnsignedShort();
+            byte version = buf.readByte();
+            int requestId = buf.readInt();
+            int bodyLength = buf.readInt();
+            int crc32 = buf.readInt(); // 这里可以不用了，只是把 readerIndex 往后挪
 
-        // 读取 body
-        byte[] body = new byte[bodyLength];
-        buf.readBytes(body);
+            // 读取 body
+            byte[] body = new byte[bodyLength];
+            buf.readBytes(body);
 
-        // 组装 GameMessage
-        GameMsgType msgType = GameMsgType.of(typeByte);
-        GameMessage gameMessage = new GameMessage(msgType, version, requestId, body);
-
-        out.add(gameMessage);
+            // 组装 GameMessage
+            GameMsgType msgType = GameMsgType.of(typeByte);
+            GameMessage gameMessage = new GameMessage(msgType, version, requestId, body);
+            out.add(gameMessage);
+        } finally {
+            ReferenceCountUtil.release(buf);
+        }
     }
 }
--- src/main/java/com/zunf/tankbattletcpserver/handler/netty/GameMessageEncoder.java
+++ src/main/java/com/zunf/tankbattletcpserver/handler/netty/GameMessageEncoder.java
@@ -37,6 +37,7 @@ public class GameMessageEncoder extends MessageToMessageEncoder<GameMessage> {
             buf.writeBytes(body);
         }
 
+        // netty 会自动释放
         out.add(buf);
     }
 }
